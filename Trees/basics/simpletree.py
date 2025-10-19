438f90b926234868826b8f1801491dca75ca6479
kuldeepdhadhwal <kuldeepdhadhwal24@gmail.com>
2025-10-19 22:19:16 +0530

    changes done

---
+++ Trees/basics/simpletree.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def insertNode(self, key):
+        if self.val:
+            if key < self.val:
+                if self.left is None:
+                    self.left = Node(key)
+                else:
+                    self.left.insertNode(key)
+            elif key > self.val:
+                if self.right is None:
+                    self.right = Node(key)
+                else:
+                    self.right.insertNode(key)
+
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.val)
+            res = res + self.inorderTraversal(root.right)
+        return res
+    def preorderTraversal(self, root):
+        res = []
+        if root:
+            res.append(root.val)
+            res = res + self.preorderTraversal(root.left)
+            res = res + self.preorderTraversal(root.right)
+        return res
+    
+
+
+n = Node(10)
+n.insertNode(5)
+n.insertNode(15)
+n.insertNode(3)
+print("Inorder Traversal:", n.inorderTraversal(n))
+print("Preorder Traversal:", n.preorderTraversal(n))
+

aa963d919e5103fa71f532e7bfc2851f445cc408
kuldeepdhadhwal <kuldeepdhadhwal24@gmail.com>
2025-11-03 14:47:59 +0530

    added two pointer approach

--- EveryDayProblem/min_time_to_make_rope_colorful.py
+++ EveryDayProblem/min_time_to_make_rope_colorful.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
         res = max_cost = 0
@@ -7,5 +8,26 @@ class Solution:
                 max_cost = 0
             res += min(max_cost, neededTime[i])
             max_cost = max(max_cost,neededTime[i])
+        
+        return res
+
+
+
+#two pointer approach
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        l = 0
+        res = 0
+
+        for r in range(1,len(colors)):
+            if colors[l] == colors[r]:
+                #check which one is min
+                if neededTime[l] < neededTime[r]:
+                    res += neededTime[l]
+                    l = r
+                else:
+                    res += neededTime[r]
+            else:
+                l = r
         
         return res

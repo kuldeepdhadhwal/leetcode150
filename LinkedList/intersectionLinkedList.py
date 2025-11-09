15e0b58290650454d5fb75c59483acfe13711c84
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-11-09 18:59:47 +0530

    Create intersectionLinkedList.py

---
+++ LinkedList/intersectionLinkedList.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        # a, b = headA, headB
+
+        # while (a!=b):
+        #     a = headB if not a else a.next
+        #     b = headA if not b else b.next
+        # return a
+
+        hash_set = set()
+
+        temp1 = headA
+        temp2 = headB
+
+        while temp1:
+            hash_set.add(temp1)
+            temp1 = temp1.next
+        
+        while temp2:
+            if temp2 in hash_set:
+                print(temp2)
+                return temp2
+            temp2 = temp2.next
+        return None

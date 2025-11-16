fce51aa61ca768771547015cc8f4b38b3b0b7db7
kuldeepdhadhwal <kuldeepdhadhwal24@gmail.com>
2025-11-16 12:14:41 +0530

    added stack

---
+++ stack/153_min_stack.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = [(-1, float('inf'))]
+
+    def push(self, val: int) -> None:
+        self.stack.append([val,min(val, self.stack[-1][1])])
+        
+
+    def pop(self) -> None:
+        if len(self.stack) > 1: self.stack.pop()
+        
+
+    def top(self) -> int:
+        if len(self.stack) == 1: return None
+        return self.stack[-1][0]
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

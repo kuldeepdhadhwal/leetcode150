8b521b6e55d6cd8b260f6bc30cf444a4d45e2c40
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-12-29 16:31:05 +0530

    Implement recursive DP solution for House Robber
    Added a recursive dynamic programming solution for the House Robber problem.

--- DynamicProgramming/HouseRobber.py
+++ DynamicProgramming/HouseRobber.py
@@ -7,3 +7,18 @@ class Solution:
             rob1 = rob2
             rob2 = max_rob
         return rob2
+
+
+# dynamic programming
+def rob(arr, i=0, lookup=None):
+    lookup = {} if lookup is None else lookup
+    if i >= len(arr):
+        return 0
+    if i in lookup:
+        return lookup[i]
+    else:
+        lookup[i] = max(arr[i]+rob(arr, i+2,lookup), rob(arr, i+1,lookup))
+        return lookup[i]
+arr = [2, 10, 3, 6, 8, 1, 7]        
+res = rob(arr)
+print(res)
